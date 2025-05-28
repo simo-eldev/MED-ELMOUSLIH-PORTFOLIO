b7b3db8a5b4bece0cbaa42be61cf6f315ca320af
simo-eldev <simoel2013@gmail.com>
2025-05-28 20:38:05 +0100

    redeploy

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio/",
-  
+  base: "/MED-ELMOUSLIH-PORTFOLIO/",
   server: {
     open: true,
   },
